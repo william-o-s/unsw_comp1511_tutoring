6b13d95c7e2b4644512730914cc3bdf5af0d767c
william-o-s <z5388080@ad.unsw.edu.au>
2023-02-16 13:12:36 +1100

    Added week 1 code files

---
+++ Week 1/hello_world.c
@@ -0,0 +1,17 @@
+// Basic Hello World program
+// Written by Marc Chee, September 2020
+// Plagiarised by William Setiawan (z5388080)
+//  on 16 February 2023
+
+// Template:
+// (DESCRIPTION)
+// Written by (NAME) (ZID)
+//  on (DATE)
+
+#include <stdio.h>
+
+int main (void) {
+    printf("Hello World\n");
+    
+    return 0;
+}
