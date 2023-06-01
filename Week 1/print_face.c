f5542f0733922dd38efd749c61dcfce827e425f8
william-o-s <z5388080@ad.unsw.edu.au>
2023-06-02 03:31:27 +1000

    Added Week 1 files

---
+++ Week 1/print_face.c
@@ -0,0 +1,32 @@
+// Prints a happy face
+// William Setiawan (z5388080)
+// on 2/6/2023
+
+#include <stdio.h>
+
+/**
+ * The face should look like:
+
+ ~ ~
+ 0 0
+  o
+ \_/
+
+ */
+
+int main(void) {
+    // This face is a little sad, and doesn't do what the program says it does.
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+
+    // TODO: Let's make the face happy here!
+    // '\n' has a backslash ('\'). What does it do? Can it still be printed?
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" -\n");
+    
+    return 0;
+}
