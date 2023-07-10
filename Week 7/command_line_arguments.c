78707172b6fe573a9cb462e6e305b66c9d2a5458
william-o-s <z5388080@ad.unsw.edu.au>
2023-07-11 01:00:30 +1000

    Added Week 7 codefiles

---
+++ Week 7/command_line_arguments.c
@@ -0,0 +1,12 @@
+// William Setiawan (z5388080)
+// on 11/7/2023
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // TODO: Print the total arguments supplied
+
+    // TODO: Print each argument
+
+    return 0;
+}
