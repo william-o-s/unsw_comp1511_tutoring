dbc7b32691891089e12b9200e8756113eb114d18
william-o-s <z5388080@ad.unsw.edu.au>
2024-03-25 19:03:56 +1100

    Added Week 7 code

---
+++ Week 7/part4_command_line_args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    printf("Total arguments: %d\n", argc);
+
+    printf("Argument values:\n");
+    for (int i = 0; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
