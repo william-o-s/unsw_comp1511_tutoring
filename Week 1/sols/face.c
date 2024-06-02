44450ac183295b87a58526c3e47a3c2af281ac6d
william-o-s <z5388080@ad.unsw.edu.au>
2024-06-02 17:13:54 +1000

    Added Week 1 solutions; Added Week 2 slides

---
+++ Week 1/sols/face.c
@@ -0,0 +1,37 @@
+// face.c
+//
+// Written by Sofia De Bellis (z5418801),
+// on Janaruary 2024
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+#include <stdio.h>
+
+int main(void) {
+    
+    // Face 1
+    printf("  ~ ~\n");
+    printf("  0 0\n");
+    printf("   o\n");
+    printf("   -\n");
+    
+    // Face 2
+    printf("  ~ ~\n");
+    printf("  0 0\n");
+    printf("   o\n");
+    printf("  \\_/\n");
+
+    return 0;
+}
