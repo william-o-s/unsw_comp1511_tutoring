684df8a8117badac38c6331f3c3e95471d5f8aa0
william-o-s <z5388080@ad.unsw.edu.au>
2024-06-23 05:14:06 +1000

    Added Week 5 slides and starter code

---
+++ Week 5/starter_code/part1_galaxy.c
@@ -0,0 +1,75 @@
+// part1_galaxy.c
+//
+// This program was written by YOUR-NAME (ZID)
+// on DATE
+//
+// This program is a simple game that allows the user to build a galaxy.
+// The user can place stars, planets, and nebulae in the galaxy.
+
+#include <stdio.h>
+
+#define SIZE 5
+#define NEBULA_POINTS -10
+
+enum entity {
+    STAR,
+    PLANET,
+    NEBULA,
+    SPACESHIP,
+    EMPTY,
+};
+
+struct celestial_body {
+    enum entity entity;
+    int points;
+};
+
+void print_map(struct celestial_body galaxy[SIZE][SIZE]);
+
+int main(void) {
+    struct celestial_body galaxy[SIZE][SIZE];
+
+    // TODO: Initialize the galaxy
+
+    // TODO: Place the planets and nebulae in the galaxy
+    printf("How many planets and nebulae are there? ");
+
+    // TODO: Place the planets and nebulae in the galaxy
+    printf("Enter the position and points of the planet(s) and nebula(e): ");
+
+    // TODO: Place the stars in the galaxy
+    printf("Enter the position and points of the star(s): \n");
+
+    // TODO: Place the player in the galaxy
+    printf("Enter the starting position of the player: ");
+
+    // TODO: Print the map
+}
+
+// Function prints the map of the galaxy
+//
+// Parameters:
+// - galaxy: the 2D array representing the galaxy
+//
+// returns: nothing
+void print_map(struct celestial_body galaxy[SIZE][SIZE]) {
+    printf("\n---------------------\n");
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("|");
+            if (galaxy[i][j].entity == SPACESHIP) {
+                printf(" X ");
+            } else if (galaxy[i][j].entity == EMPTY) {
+                printf("   ");
+            } else if (galaxy[i][j].entity == STAR) {
+                printf(" * ");
+            } else if (galaxy[i][j].entity == PLANET) {
+                printf(" o ");
+            } else if (galaxy[i][j].entity == NEBULA) {
+                printf(" # ");
+            }
+        }
+        printf("|\n");
+        printf("---------------------\n");
+    }
+}
